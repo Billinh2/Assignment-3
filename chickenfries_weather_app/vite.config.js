6a32d57d23c4e4e2b35d4e26fddc97712fc5a81d
Billinh2 <thaibinha1ntt@gmail.com>
2024-11-01 20:15:13 +1100

    update vit.config.js

--- chickenfries_weather_app/vite.config.js
+++ chickenfries_weather_app/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.VITE_PORT || 8386,
+  },
+});
