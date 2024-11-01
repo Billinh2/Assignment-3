04face6022bc08ba4240e7230988ffe8c95883ec
Billinh2 <thaibinha1ntt@gmail.com>
2024-11-02 01:09:37 +1100

    node_moduless and vite

--- chickenfries_weather_app/vite.config.js
+++ chickenfries_weather_app/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.VITE_PORT || 8386,
+  },
+});
