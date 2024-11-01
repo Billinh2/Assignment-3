48d45d82f0966545a1c34f0a027ba6afb7068f2c
ninayal <ninayal198@gmail.com>
2024-11-02 00:30:03 +1100

    nina push

---
+++ chickenfries_weather_app/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3 className="footer__text">
+        Group 50 - ChickenFries - Swinburne University of Technology
+      </h3>
+    </div>
+  );
+};
+
+export default Footer;
